b51b952453042329a3090ff3199471a048efb27d
im-engineer <azmsiddhant1@gmail.com>
2023-04-20 10:48:18 +0530

    create video player

---
+++ frontend/src/component/Projects/ProjectPlayer/ProjectPlayer.js
@@ -0,0 +1,11 @@
+import React,{useRef} from 'react';
+import ReactPlayer from 'react-player';
+
+
+function ProjectPlayer() {
+  return (
+    <div>ProjectPlayer</div>
+  )
+}
+
+export default ProjectPlayer
