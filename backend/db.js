884bc64a1892b9014c0cf24f48dc69f0e67499de
Siddhant <azmsiddhant@gmail.com>
2023-04-24 01:55:19 +0530

    changes in update

--- backend/db.js
+++ backend/db.js
@@ -3,20 +3,21 @@ import mongoose from 'mongoose';
 var config = require('./config/config');
 
 var configdata = config.get(process.env.Node_env);
+console.log("🚀 ~ file: db.js:6 ~ configdata:", configdata)
 import 'dotenv/config'
 var port = process.env.PORT || configdata.api_port
 var mongoUrl = process.env.API_Url;
 
 console.log("🚀 ~ file: db.js:10 ~ mongoUrl:", mongoUrl)
 
-var options= {
-    user:configdata.userName,
-    pass:configdata.password
-}
+// var options= {
+//     user:configdata.userName,
+//     pass:configdata.password
+// }
 
 export const mongoconnection = async() => {
     try{
-        await mongoose.connect(mongoUrl,options);
+        await mongoose.connect(mongoUrl);
         console.log("Connect to DB");
     }
     catch(e){
