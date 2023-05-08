1527cf9d65327527f331a3cc3b1350820bfe097e
im-engineer <azmsiddhant1@gmail.com>
2023-05-08 14:08:29 +0530

    changes in project

--- frontend/src/Data/projectData.js
+++ frontend/src/Data/projectData.js
@@ -2,11 +2,21 @@ import one from '../assets/svg/projects/ECG.png'
 import two from '../assets/svg/projects/OxVirtual.png'
 import five from '../assets/svg/projects/five.svg'
 import four from '../assets/svg/projects/inoice.png'
+import three from '../assets/svg/projects/three.png'
 
 
 const projectsData = [
     {
         id: 1,
+        projectName: 'Pxalm',
+        projectDesc: 'The goal of a Pxalm is to accelerate the personal and professional development of mentees. This is achieved by providing mentees with guidance, advice and feedback from mentors with more experience than themselves.',
+        tags: ['Angular','MongoDB','Node','Express'],
+        code: 'https://mean.stagingsdei.com:9193/',
+        demo: 'https://mean.stagingsdei.com:9193/',
+        image: three
+    },
+    {
+        id: 2,
         projectName: 'EKG',
         projectDesc: 'The Goal of EKG is to provide platform for patient to submit the ekg report for the review at any time in very flexible manner to save the time of patient and provider.',
         tags: ['Angular','MongoDB','Node','Express'],
@@ -15,7 +25,7 @@ const projectsData = [
         image: two
     },
     {
-        id: 2,
+        id: 3,
         projectName: 'OXVirtualCare',
         projectDesc: 'Ox Virtual Care give you access to the widest variety of market leading monitors, the best clinical user experience, and seamless integration with your other clinical systems.',
         tags: ['Angular','MongoDB','Node','Express'],
@@ -24,7 +34,7 @@ const projectsData = [
         image: one
     },
     {
-        id: 3,
+        id: 4,
         projectName: 'Shop with Siddhant',
         projectDesc: 'Shopify is e-commerce website user can shop here whatever they want it is like amazon, flipkart.',
         tags: ['React','MongoDB','Node','Express'],
@@ -33,7 +43,7 @@ const projectsData = [
         image: five
     },
     {
-        id: 4,
+        id: 5,
         projectName: 'Invoice',
         projectDesc: 'A simple invoice management system',
         tags: ['React','MongoDB','Node','Express'],
